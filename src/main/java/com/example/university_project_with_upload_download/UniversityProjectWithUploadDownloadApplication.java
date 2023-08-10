e8074aaf09237ab7cc3c5db0d47c6d093a557ab3
ZafarDevoloper <jasurake04@gmail.com>
2023-08-10 19:52:20 +0500

    first

---
+++ src/main/java/com/example/university_project_with_upload_download/UniversityProjectWithUploadDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.example.university_project_with_upload_download;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityProjectWithUploadDownloadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityProjectWithUploadDownloadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/university_project_with_upload_download/UniversityProjectWithUploadDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.university_project_with_upload_download;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityProjectWithUploadDownloadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
